8fe376da96de0b3c889483397fa1e71fa190e2a3
fuminyan-238 <fuminyan238@gmail.com>
2020-05-23 17:34:46 +0900

    fix

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,5 +2,6 @@ class CreditCard < ApplicationRecord
   belongs_to :user
   validates :card_id, presence: true
   validates :customer_id, presence: true
+  validates :user_id, presence: true
   
 end
